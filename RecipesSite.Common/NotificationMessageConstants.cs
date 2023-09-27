24ea6eccef94609108c43445a4da0364d3753368
Yanita Kyuchukova <111355885+2201280307070703@users.noreply.github.com>
2023-09-27 17:54:13 +0300

    add notification messages constants

---
+++ RecipesSite.Common/NotificationMessageConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace RecipesSite.Common
+{
+    public static class NotificationMessageConstants
+    {
+        public const string ErrorMessage = "ErrorMessage";
+
+        public const string SuccessMessage = "SuccessMessage";
+
+        public const string WarningMessage = "WarnMessage";
+
+        public const string InformationMessage = "InfoMessage";
+    }
+}
