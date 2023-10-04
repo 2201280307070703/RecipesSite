8b04b5d638b508a119c6c77d8273968262d8c4eb
Yanita Kyuchukova <111355885+2201280307070703@users.noreply.github.com>
2023-10-04 16:56:31 +0300

    Add site info and instructions for site

---
+++ RecipesSite.Web/Controllers/AboutUs.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecipesSite.Web.Controllers
+{
+    public class AboutUs : Controller
+    {
+        public IActionResult Info()
+        {
+            return View();
+        }
+
+        public IActionResult Instructions()
+        {
+            return View();
+        }
+    }
+}
